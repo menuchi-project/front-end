1fd2af273c8b03d4266c3c3a1f2be6b696f52c29
Zahra Masoumi <zahraamasoumi@gmail.com>
2025-03-21 21:50:17 +0330

    init project with antd

--- menuchi-app/src/app/app.component.spec.ts
+++ menuchi-app/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'menuchi-app' title`, () => {
+  it(`should have as title 'menuchi-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('menuchi-app');
@@ -24,8 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, menuchi-app',
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, menuchi-app');
   });
 });
