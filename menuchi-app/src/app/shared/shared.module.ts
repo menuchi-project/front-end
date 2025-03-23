fc3af742b8e6151cca89e4cc879ab76b2f063ab9
Zahra Masoumi <zahraamasoumi@gmail.com>
2025-03-23 16:08:14 +0330

    fix checkbox radius, menu items, add icons, and collapse menu item + delete button

--- menuchi-app/src/app/shared/shared.module.ts
+++ menuchi-app/src/app/shared/shared.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NzIconDirective} from 'ng-zorro-antd/icon';
+import {NzMenuDirective, NzMenuItemComponent, NzSubMenuComponent} from 'ng-zorro-antd/menu';
+import {NzSiderComponent} from 'ng-zorro-antd/layout';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, NzIconDirective, NzMenuDirective, NzMenuItemComponent, NzSiderComponent, NzSubMenuComponent],
   exports: [],
 })
-export class SharedModule {}
+export class SharedModule {
+}
