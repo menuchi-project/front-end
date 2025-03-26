16e77aaabb77fe39e528d10af6b8047afc61e329
Zahra Masoumi <zahraamasoumi@gmail.com>
2025-03-26 20:31:11 +0330

    fix breadcrumb with routing

--- menuchi-app/src/app/app-routing.module.ts
+++ menuchi-app/src/app/app-routing.module.ts
@@ -1,9 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesPageComponent } from './user/categories-page/categories-page.component';
+import { ManagementPageComponent } from './user/management-page/management-page.component';
+import { AppComponent } from './app.component';
 import { ItemsPageComponent } from './user/items-page/items-page.component';
 
 const routes: Routes = [
-  { title: 'salam', path: 'items', component: ItemsPageComponent },
+  { path: 'home', component: AppComponent, data: { breadcrumb: 'خانه' } },
+  {
+    path: 'manage',
+    component: ManagementPageComponent,
+    data: { breadcrumb: 'پنل مدیریت' },
+    children: [
+      {
+        path: '',
+        component: ItemsPageComponent,
+        data: { breadcrumb: 'داشبورد مدیریت' },
+      },
+      {
+        path: 'cats',
+        component: CategoriesPageComponent,
+        data: { breadcrumb: 'بک لاگ' },
+      },
+    ],
+  },
+  // {
+  //   path: 'manage/cats',
+  //   component: CategoriesPageComponent,
+  //   data: { breadcrumb: 'بک لاگ' },
+  // },
 ];
 
 @NgModule({
