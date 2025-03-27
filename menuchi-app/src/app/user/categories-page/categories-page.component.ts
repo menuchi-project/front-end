11c92777f9591f22ddade85fa7da677a37e7cf92
Zahra Masoumi <zahraamasoumi@gmail.com>
2025-03-27 12:33:00 +0330

    complete drag and drop 🎉

--- menuchi-app/src/app/user/categories-page/categories-page.component.ts
+++ menuchi-app/src/app/user/categories-page/categories-page.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-categories-page',
@@ -6,4 +11,48 @@ import { Component } from '@angular/core';
   templateUrl: './categories-page.component.html',
   styleUrl: './categories-page.component.scss',
 })
-export class CategoriesPageComponent {}
+export class CategoriesPageComponent {
+  lists = [
+    {
+      id: 'A',
+      items: [
+        { name: 'Item 1' },
+        { name: 'Item 2' },
+        { name: 'Item 1' },
+        { name: 'Item 2' },
+      ],
+    },
+    { id: 'B', items: [{ name: 'Item 3' }, { name: 'Item 4' }] },
+    { id: 'C', items: [{ name: 'Item 5' }, { name: 'Item 6' }] },
+    { id: 'D', items: [{ name: 'Item 7' }, { name: 'Item 8' }] },
+  ];
+
+  allConnectedLists = this.lists.map((l) => l.id);
+
+  onItemDropped(event: CdkDragDrop<any[]>) {
+    const prevIndex = this.lists.findIndex(
+      (list) => list.items === event.previousContainer.data,
+    );
+    const currIndex = this.lists.findIndex(
+      (list) => list.items === event.container.data,
+    );
+
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        this.lists[prevIndex].items,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    } else {
+      transferArrayItem(
+        this.lists[prevIndex].items,
+        this.lists[currIndex].items,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+
+    console.log(prevIndex, currIndex);
+    console.log(this.lists);
+  }
+}
--- menuchi-app/src/app/user/categories-page/category/category.component.ts
+++ menuchi-app/src/app/user/categories-page/category/category.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { Data } from '../../items-page/items-table/items-table.component';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-category',
@@ -10,40 +9,12 @@ import { Data } from '../../items-page/items-table/items-table.component';
 })
 export class CategoryComponent {
   loading: boolean = false;
-  listOfData: Data[] = [
-    {
-      id: '0',
-      name: 'Joe kkkkk',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-    },
-    {
-      id: '4',
-      name: 'Joe kd[pewk',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-    },
-    {
-      id: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-    },
-    {
-      id: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-    },
-    {
-      id: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-    },
-  ];
 
-  drop(event: CdkDragDrop<string[]>): void {
-    moveItemInArray(this.listOfData, event.previousIndex, event.currentIndex);
+  @Input() list: any;
+  @Input() connectedLists: string[] = [];
+  @Output() itemDropped = new EventEmitter<CdkDragDrop<any[]>>();
+
+  drop2(event: CdkDragDrop<any[]>) {
+    this.itemDropped.emit(event);
   }
 }
