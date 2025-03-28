aff65829efdfd2f04f828f73599b54eb5ef935f7
Zahra Masoumi <102908292+asAlwaysZahra@users.noreply.github.com>
2025-03-29 03:01:15 +0330

    Feature/backlog page (#5)

---
+++ menuchi-app/src/app/user/backlog-page/backlog-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BacklogPageComponent } from './backlog-page.component';
+
+describe('BacklogPageComponent', () => {
+  let component: BacklogPageComponent;
+  let fixture: ComponentFixture<BacklogPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BacklogPageComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BacklogPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
