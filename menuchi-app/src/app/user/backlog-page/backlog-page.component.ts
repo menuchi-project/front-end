eee712dbde4d9732c279f0b130b2384f6657a554
Zahra Masoumi <zahraamasoumi@gmail.com>
2025-03-29 17:30:21 +0330

    remove useless files

--- menuchi-app/src/app/user/backlog-page/backlog-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-backlog-page',
-  standalone: false,
-  template: '<router-outlet></router-outlet>',
-})
-export class BacklogPageComponent {}
