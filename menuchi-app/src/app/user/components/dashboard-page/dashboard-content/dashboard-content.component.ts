8cc51cd18378e53c4a7037d2391f9aab64d8156d
Zahra Masoumi <zahraamasoumi@gmail.com>
2025-05-02 00:01:39 +0330

    not completed

--- menuchi-app/src/app/user/components/dashboard-page/dashboard-content/dashboard-content.component.ts
+++ menuchi-app/src/app/user/components/dashboard-page/dashboard-content/dashboard-content.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from '../../../models/Item';
+import { ItemService } from '../../../services/item/item.service';
 
 @Component({
   selector: 'app-dashboard-content',
   standalone: false,
   templateUrl: './dashboard-content.component.html',
-  styleUrl: './dashboard-content.component.scss'
+  styleUrl: './dashboard-content.component.scss',
 })
-export class DashboardContentComponent {
+export class DashboardContentComponent implements OnInit {
+  item!: Item;
 
+  constructor(private readonly itemService: ItemService) {
+    this.itemService.itemsData$.subscribe({
+      next: (response: Item[]) => {
+        this.item = response[0];
+      },
+      error: (error) => {
+        console.log('error in dashboard, line 22:', error);
+      },
+    });
+
+    this.itemService.geAllItems();
+  }
+
+  ngOnInit() {
+    // this.itemService.itemsData$.subscribe({
+    //   next: (response: Item[]) => {
+    //     this.item = response[0];
+    //   },
+    //   error: (error) => {
+    //     console.log('error in dashboard, line 22:', error);
+    //   },
+    // });
+  }
 }
