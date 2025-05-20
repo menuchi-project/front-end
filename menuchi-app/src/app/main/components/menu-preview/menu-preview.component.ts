1cc40256b0f88e5e0bc39e9fdabe61a50bc9a426
Zahra Masoumi <zahraamasoumi@gmail.com>
2025-05-20 19:51:12 +0330

    fi cate name

--- menuchi-app/src/app/main/components/menu-preview/menu-preview.component.ts
+++ menuchi-app/src/app/main/components/menu-preview/menu-preview.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CATEGORIES } from '../../models/CatNameIcons';
 
 @Component({
   selector: 'app-menu-preview',
   standalone: false,
   templateUrl: './menu-preview.component.html',
-  styleUrl: './menu-preview.component.scss'
+  styleUrl: './menu-preview.component.scss',
 })
 export class MenuPreviewComponent {
-
+  items = CATEGORIES;
 }
--- menuchi-app/src/app/shared/components/horizontal-scroller/horizontal-scroller.component.ts
+++ menuchi-app/src/app/shared/components/horizontal-scroller/horizontal-scroller.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-horizontal-scroller',
@@ -6,38 +13,29 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./horizontal-scroller.component.scss'],
   standalone: false,
 })
-export class HorizontalScrollerComponent {
+export class HorizontalScrollerComponent implements OnChanges {
   @ViewChild('scrollContainer', { static: false }) scrollContainer!: ElementRef;
 
-  menuItems = [
-    { label: 'پیتزا', icon: 'assets/icons/categories/Pizza.svg' },
-    { label: 'پیتزا', icon: 'assets/icons/categories/Pizza.svg' },
-    { label: 'پیتزا', icon: 'assets/icons/categories/Pizza.svg' },
-    { label: 'پیتزا', icon: 'assets/icons/categories/Pizza.svg' },
-    { label: 'کباب', icon: 'assets/icons/categories/Pizza.svg' },
-    { label: 'پیتزا', icon: 'assets/icons/categories/Pizza.svg' },
-    { label: 'نوشیدنی', icon: 'assets/icons/categories/Pizza.svg' },
-    { label: 'نوشیدنی', icon: 'assets/icons/categories/Pizza.svg' },
-    { label: 'نوشیدنی', icon: 'assets/icons/categories/Pizza.svg' },
-    { label: 'نوشیدنی', icon: 'assets/icons/categories/Pizza.svg' },
-    { label: 'نوشیدنی', icon: 'assets/icons/categories/Pizza.svg' },
-    { label: 'نوشیدنی', icon: 'assets/icons/categories/Pizza.svg' },
-    { label: 'نوشیدنی', icon: 'assets/icons/categories/Pizza.svg' },
-    { label: 'نوشیدنی', icon: 'assets/icons/categories/Pizza.svg' },
-  ];
+  @Input() items: any[] = [];
 
-  selectedItem = this.menuItems[0];
+  selectedItem: any;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['items'] && this.items.length > 0) {
+      this.selectedItem = this.items[0];
+    }
+  }
 
   scrollRight() {
     this.scrollContainer.nativeElement.scrollBy({
-      left: -150,
+      left: -200,
       behavior: 'smooth',
     });
   }
 
   scrollLeft() {
     this.scrollContainer.nativeElement.scrollBy({
-      left: 150,
+      left: 200,
       behavior: 'smooth',
     });
   }
