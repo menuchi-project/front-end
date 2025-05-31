c4082092869425a3bbfad79c280704030f4f91ab
MahyaJafari <mahyajfri37@gmail.com>
2025-05-30 23:14:21 -0700

    style the second page

--- menuchi-app/src/app/user/components/otp-email-verification-page/otp-email-verification-page.component.ts
+++ menuchi-app/src/app/user/components/otp-email-verification-page/otp-email-verification-page.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../../main/services/auth/auth.service';
+import { NgForm, FormsModule } from '@angular/forms';
+import { NgOptimizedImage, CommonModule } from '@angular/common';
+import { NzInputOtpComponent } from 'ng-zorro-antd/input';
+import { NzFlexModule } from 'ng-zorro-antd/flex';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-otp-verification',
   templateUrl: './otp-email-verification-page.component.html',
-  styleUrl: './otp-email-verification-page.component.scss'
+  styleUrl: './otp-email-verification-page.component.scss',
+  standalone: true,
+  imports: [
+    FormsModule,
+    CommonModule,
+    NgOptimizedImage,
+    NzInputOtpComponent,
+    NzFlexModule,
+    NzButtonModule
+]
 })
 export class OtpVerificationComponent implements OnInit {
   otpCodeValue: string = '';
   email: string = '';
+  formatter = (value: string): string => value.toUpperCase();
 
-  constructor(private route: ActivatedRoute, private router: Router, private authService: AuthService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
@@ -24,11 +42,9 @@ export class OtpVerificationComponent implements OnInit {
     if (this.otpCodeValue.length === 6) {
       this.authService.checkOtp(this.email, this.otpCodeValue).subscribe(
         response => {
-          console.log('OTP verified for email:', this.email);
           this.router.navigate(['/dashboard']);
         },
         error => {
-          console.error('OTP verification failed:', error);
           alert('کد اشتباه است یا منقضی شده.');
         }
       );
@@ -40,11 +56,9 @@ export class OtpVerificationComponent implements OnInit {
   resendCode() {
     this.authService.sendOtp(this.email).subscribe(
       response => {
-        console.log('OTP resent:', response);
         alert('کد جدید به ' + this.email + ' ارسال شد.');
       },
       error => {
-        console.error('OTP resend failed:', error);
         alert('خطا در ارسال مجدد کد.');
       }
     );
