fdf26c5af974b2df6e0bafa6bcdc8fb91ffee15a
MahyaJafari <mahyajfri37@gmail.com>
2025-06-09 20:45:40 -0700

    style header

--- menuchi-app/src/app/user/components/created-menus-page/created-menus-page.component.ts
+++ menuchi-app/src/app/user/components/created-menus-page/created-menus-page.component.ts
@@ -3,21 +3,25 @@ import { AuthService } from '../../../main/services/auth/auth.service';
 import { MenuService } from '../../services/menu/menu.service';
 import { Menu } from '../../models/Menu';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-created-menus-page',
   standalone: false,
   templateUrl: './created-menus-page.component.html',
-  styleUrl: './created-menus-page.component.scss'
+  styleUrl: './created-menus-page.component.scss',
 })
 export class CreatedMenusPageComponent {
   menus: Menu[] = [];
+  visibleMenus: Menu[] = [];
   branchId: string | null = null;
   private subscriptions: Subscription = new Subscription();
+  searchQuery: string = '';
 
   constructor(
     private authService: AuthService,
-    private menuService: MenuService
+    private menuService: MenuService,
+    private router: Router
   ) {}
 
    ngOnInit() {
@@ -32,6 +36,7 @@ export class CreatedMenusPageComponent {
             ...menu,
             name: menu.name || 'بدون نام',
           }));
+          this.filterMenus();
         },
         error: (error) => {
           console.error('خطا در دریافت منوها:', error);
@@ -40,6 +45,20 @@ export class CreatedMenusPageComponent {
       })
     );
     this.menuService.getAllMenusForBranches();
+  }
+
+  filterMenus() {
+    if (!this.searchQuery || this.searchQuery.trim() === '') {
+      this.visibleMenus = [...this.menus];
+    } else {
+      this.visibleMenus = this.menus.filter(menu =>
+        menu.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
+  }
+
+  viewMenuDetails(menuId: string) {
+    this.router.navigate(['/dashboard/preview', menuId]);
   }
 
   ngOnDestroy() {
