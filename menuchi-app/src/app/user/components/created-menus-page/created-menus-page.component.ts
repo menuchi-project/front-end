e3392ec3d991ebd3f98985e5a82544995f3640b6
Zahra Masoumi <zahraamasoumi@gmail.com>
2025-06-28 03:29:31 +0330

    fix qr

--- menuchi-app/src/app/user/components/created-menus-page/created-menus-page.component.ts
+++ menuchi-app/src/app/user/components/created-menus-page/created-menus-page.component.ts
@@ -6,12 +6,12 @@ import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { NzMessageService } from 'ng-zorro-antd/message';
-import { environment } from '../../../../../api-config/environment'; // Import environment for API URL
-import { HttpClient } from '@angular/common/http'; // Import HttpClient
+import { environment } from '../../../../../api-config/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-created-menus-page',
-  standalone: false, // Set to true if it's meant to be standalone
+  standalone: false,
   templateUrl: './created-menus-page.component.html',
   styleUrl: './created-menus-page.component.scss',
 })
@@ -22,7 +22,6 @@ export class CreatedMenusPageComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription = new Subscription();
   searchQuery: string = '';
 
-  // Properties for QR Code Modal
   isQrModalVisible = false;
   publicMenuUrl: string | null = null;
   currentMenuName: string | null = null;
@@ -33,7 +32,7 @@ export class CreatedMenusPageComponent implements OnInit, OnDestroy {
     private router: Router,
     private modalService: NzModalService,
     private messageService: NzMessageService,
-    private http: HttpClient, // Inject HttpClient
+    private http: HttpClient,
   ) {}
 
   ngOnInit() {
@@ -82,14 +81,7 @@ export class CreatedMenusPageComponent implements OnInit, OnDestroy {
 
   publishAndGetQr(menuId: string, menuName: string) {
     this.currentMenuName = menuName;
-    // Construct the public URL for the menu view endpoint
     this.publicMenuUrl = `${environment.API_URL}/menus/${menuId}/view`;
-
-    // You might want to make an actual API call here if publishing is a separate action
-    // For now, we're just constructing the public URL directly.
-    // If 'publishing' means making the menu available, that logic should be handled by your backend
-    // and potentially a separate API call before generating the QR code.
-
     this.isQrModalVisible = true;
   }
 
@@ -113,6 +105,20 @@ export class CreatedMenusPageComponent implements OnInit, OnDestroy {
       }
     } else {
       this.messageService.error('عنصر QR کد پیدا نشد.');
+    }
+  }
+
+  copyLinkToClipboard(): void {
+    if (this.publicMenuUrl) {
+      navigator.clipboard
+        .writeText(this.publicMenuUrl)
+        .then(() => {
+          this.messageService.success('لینک کپی شد!');
+        })
+        .catch((err) => {
+          console.error('Failed to copy text: ', err);
+          this.messageService.error('خطا در کپی لینک.');
+        });
     }
   }
 
