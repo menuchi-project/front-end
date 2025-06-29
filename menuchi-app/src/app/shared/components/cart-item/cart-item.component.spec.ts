0c823e3e59eb53a65bb3cd551d6c5d9c0fd148a4
Zahra Masoumi <zahraamasoumi@gmail.com>
2025-06-29 10:19:32 +0330

    fix tests

--- menuchi-app/src/app/shared/components/cart-item/cart-item.component.spec.ts
+++ menuchi-app/src/app/shared/components/cart-item/cart-item.component.spec.ts
@@ -1,19 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CartItemComponent } from './cart-item.component';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { PersianNumberPipe } from '../../pipes/persian-number/persian-number.pipe';
+import { TruncatePipe } from '../../pipes/truncate/truncate.pipe';
+import { PersianPricePipe } from '../../pipes/persian-price/persian-price.pipe';
 
 describe('CartItemComponent', () => {
-  let component: CartItemComponent;
-  let fixture: ComponentFixture<CartItemComponent>;
+  let component!: CartItemComponent;
+  let fixture!: ComponentFixture<CartItemComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CartItemComponent]
-    })
-    .compileComponents();
+      declarations: [
+        CartItemComponent,
+        PersianNumberPipe,
+        TruncatePipe,
+        PersianPricePipe,
+      ],
+      imports: [NzCardModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CartItemComponent);
     component = fixture.componentInstance;
+
+    component.item = {
+      id: '1',
+      createdAt: '',
+      updatedAt: '',
+      deletedAt: null,
+      categoryId: 'cat1',
+      subCategoryId: 'subcatA',
+      categoryName: 'Category One',
+      name: 'Test Item',
+      ingredients: 'Ingredient 1, Ingredient 2',
+      price: 100,
+      picUrl: 'https://placehold.co/150x150/png?text=Item',
+      positionInItemsList: 1,
+      positionInCategory: 1,
+      orderCount: 5,
+    };
+    component.quantity = 1;
     fixture.detectChanges();
   });
 
--- menuchi-app/src/app/user/components/cart-page/cart-page.component.spec.ts
+++ menuchi-app/src/app/user/components/cart-page/cart-page.component.spec.ts
@@ -8,33 +8,52 @@ import { NzListModule } from 'ng-zorro-antd/list';
 import { NzImageModule } from 'ng-zorro-antd/image';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { NzEmptyModule } from 'ng-zorro-antd/empty';
-import { SharedModule } from "../../../shared/shared.module";
+import { SharedModule } from '../../../shared/shared.module';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 describe('CartPageComponent', () => {
   let component: CartPageComponent;
   let fixture: ComponentFixture<CartPageComponent>;
 
+  const mockActivatedRoute = {
+    paramMap: of({
+      get: (key: string) => {
+        if (key === 'menuId') {
+          return 'testMenuId';
+        }
+        return null;
+      },
+    }),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        CartPageComponent,
         NzCardModule,
         NzListModule,
         NzImageModule,
         NzTypographyModule,
         NzEmptyModule,
-        SharedModule
+        NzIconModule,
+        SharedModule,
       ],
+      declarations: [CartPageComponent],
       providers: [
         provideHttpClient(),
         provideHttpClientTesting(),
-        { 
-          provide: AuthService, 
-          useValue: { 
+        {
+          provide: AuthService,
+          useValue: {
             getCustomerEmail: () => 'test@example.com',
-          } 
-        }
-      ]
+          },
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: mockActivatedRoute,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CartPageComponent);
